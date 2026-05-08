8e3921d19ed69c80c63b9458eb6dcf870aa2dbc8
Haruki Kawasaki <h.kawasaki.cingroup2012@gmail.com>
2026-05-08 10:23:14 +0900

    Adminテーブル定義に対応するModelクラスの実装

---
+++ src/main/java/com/lesson/memo/model/Admin.java
@@ -0,0 +1,45 @@
+package com.lesson.memo.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "last_name", nullable = false, length = 255)
+    private String lastName;
+    
+    @Column(name = "first_name", nullable = false, length = 255)
+    private String firstName;
+
+    @Email(message = "メールアドレスを入力してください")
+    @NotBlank()
+    @Column(unique = true, nullable = false, length = 255)
+    private String email;
+
+    @NotBlank(message = "パスワードを入力してください")
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+}
